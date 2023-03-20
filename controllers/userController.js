42cd434bcc810045a44673b5f62f78254bc4c445
sachinsingh2020 <sachinthegreat1001@gmail.com>
2023-03-20 19:38:27 +0530

    fifth

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,7 +59,7 @@ export const login = catchAsyncError(async (req, res, next) => {
   if (!isMatch)
     return next(new ErrorHandler("Incorrect Email or Password", 401));
 
-  sendToken(res, user, `Welcome back, ${user.name}`, 200);
+  sendToken(res, user, `Welcome back, ${user.firstName}`, 200);
 })
 
 export const logout = catchAsyncError(async (req, res, next) => {
